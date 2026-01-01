745e74f56600887e2b6218991b049bea62073643
DanielChoongWeiHern <danielchoong712@gmail.com>
2026-01-01 18:41:11 +0800

    flake8 fix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import os
-#Save before running
+# Save before running
 app = Flask(__name__)
 
 
@@ -12,3 +12,4 @@ def hello():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+    
